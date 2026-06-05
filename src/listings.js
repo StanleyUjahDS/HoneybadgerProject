ffc20db6e8065bcafd271ff55dcff14e5b42c118
Stanley Ujah <stanley@Stanleys-MacBook-Pro.local>
2026-06-04 17:32:42 -0700

    Add custom domain

--- src/listings.js
+++ src/listings.js
@@ -1,35 +1,35 @@
 import React from "react";
-import { FaCoins } from "react-icons/fa";
 import "./styles/platforms.css";
 
 export default function TokenPlatforms() {
- const platforms = [
-  {
-    name: "PancakeSwap",
-    url: "#",
-    image: `${process.env.PUBLIC_URL}/images/pancakeswap-cake-logo.png`,
-  },
-  {
-    name: "CoinGecko",
-    url: "#",
-    image: `${process.env.PUBLIC_URL}/images/gecko-405ed53b475f61244130f95742a07da15f7ac30feeed5072812ae5c2d73b6194.svg`,
-  },
-  {
-    name: "CoinMarketCap",
-    url: "#",
-    image: `${process.env.PUBLIC_URL}/images/coinmarketcap.png`,
-  },
-  {
-    name: "DEXTools",
-    url: "#",
-    image: `${process.env.PUBLIC_URL}/images/dextools-seeklogo.png`,
-  },
-  {
-    name: "DexScreener",
-    url: "#",
-    image: `${process.env.PUBLIC_URL}/images/dex-screener-seeklogo.png`,
-  },
-];
+  const platforms = [
+    {
+      name: "PancakeSwap",
+      url: "#",
+      image: `${process.env.PUBLIC_URL}/images/pancakeswap-cake-logo.png`,
+    },
+    {
+      name: "CoinGecko",
+      url: "#",
+      image: `${process.env.PUBLIC_URL}/images/gecko-405ed53b475f61244130f95742a07da15f7ac30feeed5072812ae5c2d73b6194.svg`,
+    },
+    {
+      name: "CoinMarketCap",
+      url: "#",
+      image: `${process.env.PUBLIC_URL}/images/coinmarketcap.png`,
+    },
+    {
+      name: "DEXTools",
+      url: "#",
+      image: `${process.env.PUBLIC_URL}/images/dextools-seeklogo.png`,
+    },
+    {
+      name: "DexScreener",
+      url: "#",
+      image: `${process.env.PUBLIC_URL}/images/dex-screener-seeklogo.png`,
+    },
+  ];
+
   return (
     <section className="PlatformsSection">
       <h2 className="PlatformsTitle">
@@ -43,7 +43,13 @@ export default function TokenPlatforms() {
             className="PlatformCard"
             onClick={() => window.open(platform.url, "_blank")}
           >
-            <img src={platform.image} alt={platform.name} className="PlatformIcon" />
+            <div className="PlatformLogoWrapper">
+              <img
+                src={platform.image}
+                alt={platform.name}
+                className="PlatformLogo"
+              />
+            </div>
 
             <span className="PlatformName">
               {platform.name}
